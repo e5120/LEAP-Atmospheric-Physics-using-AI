8b809fb5994a2824feaa6fd8f86ed8396ceda325
e5120 <e5120@gm.kochi-ct.jp>
2024-06-09 00:27:31 +0900

    update: 各種定数を定義

--- leap/utils/__init__.py
+++ leap/utils/__init__.py
@@ -5,6 +5,12 @@ from leap.utils.utils import (
     normalize,
 )
 
+NUM_GRID = 384
+DOWN_SAMPLING = 7
+DATA_INTERVAL = 20  # [/minute]
+HOUR = 60 / DATA_INTERVAL  # [point/hour]
+DAY = 24 * HOUR # [point/day]
+YEAR = 365 * DAY  # [point/year]
 
 NUM_TRAIN = 10_091_520
 NUM_TEST = 625_000
