d6eeb778da16ac169accec87f75d0dac0d8b62b3
e5120 <e5120@gm.kochi-ct.jp>
2024-06-09 00:27:05 +0900

    fix: 推論時にエラーが出ないように修正

--- leap/dataset/single_dataset.py
+++ leap/dataset/single_dataset.py
@@ -1,11 +1,14 @@
+import torch
 from torch.utils.data import Dataset
 
 
 class SingleDataset(Dataset):
     def __init__(self, df, feat_columns, label_columns, stage="train"):
         assert stage in ["train", "val", "test"]
-        self.x_df = df[feat_columns].to_numpy()
-        self.y_df = df[label_columns].to_numpy()
+        self.df = df
+        self.x_df = torch.from_numpy(df[feat_columns].to_numpy())
+        if stage != "test":
+            self.y_df = torch.from_numpy(df[label_columns].to_numpy())
         self.stage = stage
 
     def __len__(self):
