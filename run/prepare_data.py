849ca2afac5228e151756bd0e3f4ce3668bd4665
e5120 <e5120@gm.kochi-ct.jp>
2024-06-13 15:53:41 +0900

    update: データ分割の高速化&前処理の廃止

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import hydra
 import polars as pl
+import pandas as pd
+import pyarrow as pa
+from pyarrow import csv
 from tqdm.auto import tqdm
 
 from leap.utils import NUM_TRAIN, IN_SCALAR_COLUMNS, IN_VECTOR_COLUMNS, OUT_SCALAR_COLUMNS, OUT_VECTOR_COLUMNS
@@ -12,42 +15,37 @@ def split_dataset(cfg):
     output_dir = Path(cfg.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     print("splitting dataset")
-    weight_df = pl.read_csv(Path(data_dir, "sample_submission.csv"), n_rows=1)
-    weight = weight_df[:, 1:].to_pandas().T.to_dict()[0]
-    sample_df = pl.read_csv(Path(data_dir, "train.csv"), n_rows=100_000)
-    gs = (NUM_TRAIN - 1) // cfg.batch_size + 1
-    for i in tqdm(range(gs)):
-        start = i * cfg.batch_size
-        end = (i + 1) * cfg.batch_size - 1
-        df = pl.read_csv(
-            Path(data_dir, "train.csv"),
-            n_rows=cfg.batch_size,
-            skip_rows=start,
-            schema=sample_df.schema,
-        )
-        df = df.with_columns(
-            [
-                pl.col(col) * weight[col]
-                for col in weight_df.columns[1:]
-            ]
-        )
-        df.write_parquet(Path(output_dir, f"{cfg.prefix}train_{start}_{end}.parquet"))
+    reader = pa.csv.open_csv(Path(data_dir, "train.csv"))
+    n_data = 0
+    start = n_data
+    dfs = []
+    for batch in tqdm(reader):
+        n_data += len(batch)
+        df = pa.Table.from_batches([batch]).to_pandas()
+        dfs.append(df)
+        if n_data >= cfg.batch_size:
+            df = pd.concat(dfs).reset_index(drop=True)
+            end = start + n_data - 1
+            df.to_parquet(Path(output_dir, f"raw_train_{start}_{end}.parquet"))
+            start += n_data
+            n_data = 0
+            dfs = []
     test_df = pl.read_csv(Path(data_dir, "test.csv"))
-    test_df.write_parquet(Path(output_dir, f"{cfg.prefix}test.parquet"))
+    test_df.write_parquet(Path(output_dir, "raw_test.parquet"))
 
 
 def generate_sequential_data(cfg):
     data_dir = Path(cfg.data_dir)
     print("generating dataset")
     # train data
-    for filename in tqdm(sorted(list(data_dir.glob(f"{cfg.prefix}train*")))):
+    for filename in tqdm(sorted(list(data_dir.glob("raw_train*")))):
         df = pl.read_parquet(filename)
         for col in IN_VECTOR_COLUMNS + OUT_VECTOR_COLUMNS:
             df = df.with_columns(pl.concat_list(f"^{col}_\d+$").alias(col))
         df = df.select(["sample_id"] + IN_SCALAR_COLUMNS + IN_VECTOR_COLUMNS + OUT_SCALAR_COLUMNS + OUT_VECTOR_COLUMNS)
         df.write_parquet(filename)
     # test data
-    filename = Path(data_dir, f"{cfg.prefix}test.parquet")
+    filename = Path(data_dir, "raw_test.parquet")
     test_df = pl.read_parquet(filename)
     for col in IN_VECTOR_COLUMNS:
         test_df = test_df.with_columns(pl.concat_list(f"^{col}_\d+$").alias(col))
