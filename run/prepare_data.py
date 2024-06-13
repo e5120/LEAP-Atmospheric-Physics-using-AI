0634440b5705ff5709fe1f9dcfec58d9bf2795c2
e5120 <e5120@gm.kochi-ct.jp>
2024-06-13 17:39:20 +0900

    update: 正規化処理とシーケンス処理を前処理に統合

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -7,13 +7,23 @@ import pyarrow as pa
 from pyarrow import csv
 from tqdm.auto import tqdm
 
-from leap.utils import NUM_TRAIN, IN_SCALAR_COLUMNS, IN_VECTOR_COLUMNS, OUT_SCALAR_COLUMNS, OUT_VECTOR_COLUMNS
+from leap.utils import (
+    NUM_TRAIN,
+    IN_SCALAR_COLUMNS,
+    IN_VECTOR_COLUMNS,
+    OUT_SCALAR_COLUMNS,
+    OUT_VECTOR_COLUMNS,
+    normalize,
+)
 
 
 def split_dataset(cfg):
     data_dir = Path(cfg.data_dir)
     output_dir = Path(cfg.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
+    if list(data_dir.glob("raw_train*")):
+        print("already exist")
+        return
     print("splitting dataset")
     reader = pa.csv.open_csv(Path(data_dir, "train.csv"))
     n_data = 0
@@ -34,23 +44,30 @@ def split_dataset(cfg):
     test_df.write_parquet(Path(output_dir, "raw_test.parquet"))
 
 
-def generate_sequential_data(cfg):
+def generate_dataset(cfg):
     data_dir = Path(cfg.data_dir)
+    if not cfg.overwrite and list(data_dir.glob(f"{cfg.prefix}train*")):
+        print("already exist")
+        return
     print("generating dataset")
+    label_cols = pl.read_csv(Path(data_dir, "sample_submission.csv"), n_rows=1).columns[1:]
+    feat_cols = pl.read_csv(Path(data_dir, "train.csv"), n_rows=1).select(pl.exclude(label_cols)).columns[1:]
     # train data
     for filename in tqdm(sorted(list(data_dir.glob("raw_train*")))):
         df = pl.read_parquet(filename)
+        df = normalize(df, feat_cols, label_cols, cfg.scaler, data_dir)
         for col in IN_VECTOR_COLUMNS + OUT_VECTOR_COLUMNS:
             df = df.with_columns(pl.concat_list(f"^{col}_\d+$").alias(col))
         df = df.select(["sample_id"] + IN_SCALAR_COLUMNS + IN_VECTOR_COLUMNS + OUT_SCALAR_COLUMNS + OUT_VECTOR_COLUMNS)
-        df.write_parquet(filename)
+        df.write_parquet(str(filename).replace("raw_", cfg.prefix))
     # test data
     filename = Path(data_dir, "raw_test.parquet")
     test_df = pl.read_parquet(filename)
+    test_df = normalize(test_df, feat_cols, [], cfg.scaler, data_dir)
     for col in IN_VECTOR_COLUMNS:
         test_df = test_df.with_columns(pl.concat_list(f"^{col}_\d+$").alias(col))
     test_df = test_df.select(["sample_id"] + IN_SCALAR_COLUMNS + IN_VECTOR_COLUMNS)
-    test_df.write_parquet(filename)
+    test_df.write_parquet(str(filename).replace("raw_", cfg.prefix))
 
 
 @hydra.main(config_path="conf", config_name="prepare_data", version_base=None)
@@ -59,8 +76,8 @@ def main(cfg):
         cfg.output_dir = cfg.data_dir
     if cfg.phase == "split":
         split_dataset(cfg)
-    if cfg.phase == "sequential":
-        generate_sequential_data(cfg)
+    if cfg.phase == "generate":
+        generate_dataset(cfg)
 
 
 if __name__=="__main__":
