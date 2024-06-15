ba369204ed402db8f4137fc114110fb2853a58ac
e5120 <e5120@gm.kochi-ct.jp>
2024-06-15 12:03:55 +0900

    add: LSMTモデルの追加

--- leap/model/__init__.py
+++ leap/model/__init__.py
@@ -2,3 +2,4 @@ from leap.model.base_model import BaseModel
 from leap.model.ffn_model import FFNModel
 from leap.model.cnn_model import CNNModel
 from leap.model.light_cnn_model import LightCNNModel
+from leap.model.lstm_model import LSTMModel
