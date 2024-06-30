dcff08cf50f14902033bbe84d1e1d3601d8c5816
ShimazuSho <shimazu.sho.jri@outlook.jp>
2024-06-30 10:07:43 +0900

    update: 損失関数をMSEとMAEの混合に変更&アテンションベースのモデルのアーキテクチャを改良

--- leap/model/attn_model.py
+++ leap/model/attn_model.py
@@ -28,8 +28,7 @@ class AttnModel(BaseModel):
     def __init__(self, input_size, output_size, num_scalar_feats, num_vector_feats, d_model=512, nhead=8, dim_feedforward=2048, num_layers=6, hidden_sizes=[256,512,512,256], p=0.1, pos_type="embedding", ignore_mask=None):
         super().__init__(ignore_mask=ignore_mask)
         assert pos_type in ["embedding", "sinusoid"]
-        self.scalar_embedding = MLPBlock(num_scalar_feats, d_model, hidden_sizes=[], p=p)
-        self.vector_embedding = MLPBlock(num_vector_feats, d_model, hidden_sizes=[], p=p)
+        self.embedding = MLPBlock(num_scalar_feats+num_vector_feats, d_model, hidden_sizes=[], p=p)
         self.pos_type = pos_type
         if pos_type == "embedding":
             self.positional_embedding = nn.Embedding(60, d_model)
@@ -48,13 +47,16 @@ class AttnModel(BaseModel):
 
     def forward(self, batch):
         bs = batch["x_scalar"].size(0)
-        scalar_emb = self.scalar_embedding(batch["x_scalar"]).unsqueeze(dim=1)  # (bs, 1, d_model)
-        vector_emb = self.vector_embedding(batch["x_vector"].permute(0, 2, 1))  # (bs, 60, d_model)
+        v = batch["x_vector"]                                             # (bs,  9, 60)
+        s = batch["x_scalar"].unsqueeze(dim=-1).repeat(1, 1, v.size(-1))  # (bs, 16, 60)
+        x = torch.cat([v, s], dim=1)                                      # (bs, 25, 60)
+        x = x.permute(0, 2, 1)
+        emb = self.embedding(x)
         if self.pos_type == "embedding":
             pos_emb = self.positional_embedding(torch.arange(60).to(batch["x_scalar"].device)).unsqueeze(dim=0)   # (1, 60, d_model)
-            emb = scalar_emb + vector_emb + pos_emb
+            emb = emb + pos_emb
         else:
-            emb = self.positional_embedding(scalar_emb + vector_emb)
+            emb = self.positional_embedding(emb)
         out = self.encoder(emb)
         out = self.decoder(out)
         out = out.permute(0, 2, 1)
--- leap/model/base_model.py
+++ leap/model/base_model.py
@@ -4,10 +4,12 @@ import torch.nn as nn
 
 
 class BaseModel(nn.Module):
-    def __init__(self, ignore_mask=None):
+    def __init__(self, ignore_mask=None, alpha=0.5):
         super().__init__()
         self.ignore_mask = ignore_mask
-        self.loss_fn = nn.MSELoss(reduction="none")
+        self.alpha = alpha
+        self.mse_loss_fn = nn.MSELoss(reduction="none")
+        self.mae_loss_fn = nn.L1Loss(reduction="none")
 
     @abstractmethod
     def forward(self, batch):
@@ -15,9 +17,13 @@ class BaseModel(nn.Module):
 
     def calculate_loss(self, batch):
         output = self.forward(batch)
-        loss = self.loss_fn(output["logits"], batch["labels"])
+        mse_loss = self.mse_loss_fn(output["logits"], batch["labels"])
+        mae_loss = self.mae_loss_fn(output["logits"], batch["labels"])
         if self.ignore_mask is not None:
-            loss = loss[:, self.ignore_mask]
-        loss = loss.mean()
+            mse_loss = mse_loss[:, self.ignore_mask]
+            mae_loss = mae_loss[:, self.ignore_mask]
+        mse_loss = mse_loss.mean()
+        mae_loss = mae_loss.mean()
+        loss = self.alpha * mse_loss + (1 - self.alpha) * mae_loss
         output["loss"] = loss
         return output
